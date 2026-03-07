35c9557e3463ddf82845a2d9c28196efe406098e
filippozuppini <zuppini.filippo@gmail.com>
2026-03-07 22:25:51 +0100

    fix Palindroma

--- main.js
+++ main.js
@@ -9,26 +9,37 @@
 
 function isPalindroma (){
     //chiedo all'utente la parola
+    //const userWord = 'Angolo bar a Bologna'; //test
     const userWord = prompt('Inserisci una parola');
+    // console.log(userWord);
     
     // const userWord = 'anna'; //esempio
+  
+    //imposto lowercase su tutte le lettere
+    let lowerUserWord = userWord.toLowerCase();
+    // console.log(lowerUserWord);
+    
 
+    //tolgo gli spazzi tra le parole
+    let trimmedUserWord = lowerUserWord.replaceAll(' ', '');
+    // console.log(trimmedUserWord);
+    
     //divido la parola in singole lettere
-    const splitUserWord = userWord.split("");
+    const splitUserWord = trimmedUserWord.split("");
+    // console.log(splitUserWord);
+    
     //roverscio l'ordine della stringa
     let reverseUserWord = splitUserWord.reverse();
+    // console.log(reverseUserWord);
+    
     //riunisco le lettere per foramre una stringa in ordine contrartio
     let joinedUserWord = reverseUserWord.join('');
-    //tolgo gli spazzi tra le parole
-    let trimmedUserWord = joinedUserWord.replaceAll(' ', '');
-    //imposto lowercase su tutte le lettere
-    let lowerUserWord = trimmedUserWord.toLowerCase();
+    // console.log(joinedUserWord + 'joined');
+    
 
-
-    // console.log(lowerUserWord);
 
     //se la parola dell'utente é uguale alla sua versione al contrario
-    if (userWord === lowerUserWord){
+    if (joinedUserWord === trimmedUserWord){
         // allora é una frase palindroma
         console.log(userWord + ' é una frase palindroma');
     } else {
@@ -45,13 +56,7 @@ isPalindroma();
 
 
 
-
-
-
-
-
-
-
+/*
 
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -59,4 +64,50 @@ isPalindroma();
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+
+
+
+
+
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// const userPoD = prompt('Pari o Dispari?');
+const userPoD = 'pari';
+
+console.log(userPoD);
+
+// const userNum = prompt('Inserisci il numero da 1 a 5');
+const userNum = 2;
+
+console.log(userNum);
+
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function computerNum() {
+    let randomNum = Math.floor((Math.random() * 5) + 1);
+    console.log(randomNum); 
+    
+}
+computerNum();
+
+// Sommiamo i due numeri
+let somma = computerNum + userNum;
+console.log(somma);
+
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function isPariOrDisapri(somma) {
+    if (somma%2 === 0) {
+        console.log('Il numero é pari');
+    } else {
+        console.log('Il numero é dispari');
+        
+    }
+}
+
+isPariOrDisapri(somma)
+
+
+*/
 
